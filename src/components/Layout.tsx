9e2703a9560b3c30de7c6526431af66c96107b88
MrJeleika <den932005@gmail.com>
2023-06-12 13:02:00 +0300

    Fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,6 +5,35 @@ import { Navbar } from "./Navbar/Navbar";
 interface Props {
   children: React.ReactNode;
 }
+export const metadata: Metadata = {
+  title: "Token minter",
+  description: "Spl token minter on Solana blockchain",
+  generator: "Next.js",
+  applicationName: "Token minter",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Solana token",
+    "Token",
+    "Solana",
+    "spl token",
+    "spl",
+    "mint token",
+    "mint solana token",
+    "mint spl",
+    "spl mint",
+  ],
+  authors: [
+    { name: "MrJeleika", url: "https://github.com/MrJeleika" },
+    { name: "Paradise ", url: "https://discord.gg/Wn2EqX9r5Y" },
+  ],
+  creator: "Paradise DAO",
+  publisher: "Paradise DAO",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+};
 
 const StyledWrapper = styled.div`
   min-height: 100vh;
