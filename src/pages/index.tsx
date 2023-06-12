f06bb061ce6d4a5d59e82df6d0742011bf6ccf1a
MrJeleika <den932005@gmail.com>
2023-06-12 13:02:13 +0300

    Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,36 +6,6 @@ import styled from "styled-components";
 
 const vt = VT323({ subsets: ["latin"], weight: "400" });
 
-export const metadata: Metadata = {
-  title: "Token minter",
-  description: "Spl token minter on Solana blockchain",
-  generator: "Next.js",
-  applicationName: "Token minter",
-  referrer: "origin-when-cross-origin",
-  keywords: [
-    "Solana token",
-    "Token",
-    "Solana",
-    "spl token",
-    "spl",
-    "mint token",
-    "mint solana token",
-    "mint spl",
-    "spl mint",
-  ],
-  authors: [
-    { name: "MrJeleika", url: "https://github.com/MrJeleika" },
-    { name: "Paradise ", url: "https://discord.gg/Wn2EqX9r5Y" },
-  ],
-  creator: "Paradise DAO",
-  publisher: "Paradise DAO",
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
-};
-
 const StyledMain = styled.main`
   height: 100%;
   background-color: ${(props) => props.theme.main};
